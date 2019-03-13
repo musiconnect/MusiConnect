7428ee52d97d77fe1902287dbb504664d0e9777a
Daniel Loftus <dcloftus@hotmail.com>
2019-03-13 18:13:55 -0400

    addedimage

---
+++ react-client/src/Components/pages/calenderPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class CalederPage extends Component {
+  render() {
+    return (
+      <div>
+        Calender Page
+      </div>
+    )
+  }
+}
+export default CalenderPage;
