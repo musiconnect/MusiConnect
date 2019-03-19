e051e15c5902c5e4b43f740c4b6f959a3cab8b47
Daniel Loftus <dcloftus@hotmail.com>
2019-03-19 15:42:49 -0400

    Pretty

--- react-client/src/Components/App.jsx
+++ react-client/src/Components/App.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import { browserHistory } from 'react-router';
+import NavBar from './HeaderComponent/NavBar';
 import HomePage from './pages/HomePage';
 import CalenderPage from './pages/calenderPage';
-import NavBar from './HeaderComponent/NavBar';
 //import id_token from google.oauth2
 
 class App extends Component {
@@ -11,13 +11,10 @@ class App extends Component {
     return (
       <Router>
         <div>
-            <div className="logo-container">
-                <img src="../images/Logo.png" alt="Musiconnect Logo"/>
-            </div>
             <div>
                 <NavBar />
-                <Route name="home" exact path="/homePage" component={HomePage} />
-                <Route name="calender" exact path="/calenderPage" component={CalenderPage} />
+                <Route name="home" path="/homePage" component={HomePage} />
+                <Route name="calender" path="/calenderPage" component={CalenderPage} />
             </div>
         </div>
       </Router>
--- react-client/src/Components/headerComponent/navBar.js
+++ react-client/src/Components/headerComponent/navBar.js
@@ -4,10 +4,16 @@ class NavBar extends Component {
   render() {
     return (
       <header>
-        <ul id="headerButtons">
-          <li className="navButton"><Link to="/HomePage/">HOME</Link></li>
-          <li className="navButton"><Link to="/CalenderPage/">CALENDER</Link></li>
-        </ul>
+        <div className="logo-container">
+            <img src="../images/Logo.png" alt="Musiconnect Logo"/>
+        </div>
+        <div classname="nav-container">
+            <ul id="headerButtons">
+              <li className="navButton"><Link to="/HomePage/">HOME</Link></li>
+              <li className="navButton"><Link to="/CalenderPage/">CALENDER</Link></li>
+            </ul>
+        </div>
+        
       </header>
     )
   }
